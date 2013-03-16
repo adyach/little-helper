36f3930e46cf92c9c4e3e72a1b48d25a4b877b14
andrey.dyachkov@gmail.com <andrey.dyachkov@gmail.com@b1d978cc-7340-0488-b184-fa4f15ed1ec9>
2013-03-16 17:24:01 +0000

    First release :)

---
+++ branches/LittleHelper 0.1/src/littlehelper/connection/CommandSender.java
@@ -0,0 +1,36 @@
+package littlehelper.connection;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+class CommandSender implements Runnable {
+
+	private String command;
+	private final OutputStream output;
+
+	CommandSender(OutputStream output) {
+		this.output = output;
+	}
+
+	@Override
+	public void run() {
+			try {
+				output.write(command.getBytes());
+				output.flush();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		// try {
+		// writer.write(command, 0, command.length());
+		// writer.flush();
+		// bout.writeTo(output);
+		// bout.reset();
+		// } catch (IOException e) {
+		// e.printStackTrace();
+		// }
+	}
+
+	void addCommand(String command) {
+		this.command = command;
+	}
+}
---
+++ branches/LittleHelper 0.1/src_controller/engine_motion_cmds/engine_motion_cmds.ino
@@ -0,0 +1,72 @@
+char buffer[9];
+byte commandSize = 8;
+
+byte leftEngine = 9;
+byte rightEngine = 10;
+
+String recievedCmd;
+String cmdStop = "00000000";
+String cmdStraight = "00000001";
+String cmdLeft = "00000010";
+String cmdRight = "00000011";
+String cmdSlowStraight = "10000001";
+String cmdSlowLeft = "10000010";
+String cmdSlowRight = "10000011";
+  
+void setup() {
+  Serial.begin(9600);
+  Serial.setTimeout(1000);
+}
+  
+byte fullStop = 0;
+byte slowStart = 70;
+int fullStart = 255;
+  
+void loop() {
+    
+}
+
+void serialEvent() {
+  if (Serial.available() > 0) {
+      int n = Serial.readBytesUntil('\n', buffer, commandSize);
+      if (n >= 0) {
+        // set end of the string
+        buffer[n] = '\0';
+        recievedCmd = buffer;
+        // send response to server
+        Serial.print(recievedCmd);
+        Serial.flush();
+      }
+    
+      if (recievedCmd == cmdStop) 
+      {
+        analogWrite(leftEngine, fullStop);  
+        analogWrite(rightEngine, fullStop);
+      } else if (recievedCmd == cmdStraight) 
+      {   
+        analogWrite(leftEngine, fullStart);  
+        analogWrite(rightEngine, fullStart);  
+      } else if (recievedCmd == cmdLeft) 
+      {   
+        analogWrite(leftEngine, fullStop);  
+        analogWrite(rightEngine, fullStart);  
+      } else if (recievedCmd == cmdRight) 
+      {   
+        analogWrite(leftEngine, fullStart);  
+        analogWrite(rightEngine, fullStop);  
+      } else if (recievedCmd == cmdSlowStraight) 
+      {   
+        analogWrite(leftEngine, slowStart);  
+        analogWrite(rightEngine, slowStart);
+      } else if (recievedCmd == cmdSlowLeft) 
+      {   
+        analogWrite(leftEngine, fullStop);  
+        analogWrite(rightEngine, fullStart);  
+      } else if (recievedCmd == cmdSlowRight) 
+      {   
+        analogWrite(leftEngine, fullStart);  
+        analogWrite(rightEngine, fullStop); 
+      }      
+  }
+}
+
